188b58aca3ed53efb32295631b379d6c7b71028a
nakaosensei <nakaosensei@gmail.com>
2019-07-07 15:27:22 -0300

    twitter streams beta

--- spark_publisher.py
+++ spark_publisher.py
@@ -22,9 +22,21 @@
 
 from confluent_kafka import Producer
 import sys
+import socket
 from time import sleep
 from json import dumps
 from kafka import KafkaProducer
+import tweepy
+
+producer = None
+
+class MyStreamListener(tweepy.StreamListener):
+    def on_status(self, status):
+        print(status.text)
+        data = {'number': status.text}
+        producer.send('twitterEng', value=data)
+        sleep(5)
+
 
 if __name__ == '__main__':
     # if len(sys.argv) != 3:
@@ -35,9 +47,7 @@ if __name__ == '__main__':
     # See https://github.com/edenhill/librdkafka/blob/master/CONFIGURATION.md
 
     # Create Producer instance
-    producer = KafkaProducer(bootstrap_servers=['localhost:9092'],
-                             value_serializer=lambda x:
-                             dumps(x).encode('utf-8'))
+    producer = KafkaProducer(bootstrap_servers=['localhost:9092'],value_serializer=lambda x:dumps(x).encode('utf-8'))
     # Optional per-message delivery callback (triggered by poll() or flush())
     # when a message has been successfully delivered or permanently
     # failed delivery (after retries).
@@ -50,16 +60,43 @@ if __name__ == '__main__':
                              (msg.topic(), msg.partition(), msg.offset()))
 
     # Read lines from stdin, produce each line to Kafka
-    for e in range(1000):
+
+    myStreamListener = MyStreamListener()
+    auth = tweepy.OAuthHandler('ZfyYT762f8iptmyOAQiPVpSSG', 'abdgza7zIl2lbRaYdY4L7C5zhGgxftLiACt5WJfrFtGnNIQqIF')
+    auth.set_access_token('188507287-370vFKYm0sA6OumKatEWXRZuA4ippzCl7kteJCpi',
+                          'MIaKNuYorBD5qqsxzzb58gOuvrBuGBToya5nzLEHsJ8kT')
+
+    api = tweepy.API(auth)
+    myStream = tweepy.Stream(auth=api.auth, listener=myStreamListener)
+
+    myStream.filter(track=['@jairbolsonaro'])
+
+    '''TCP_IP = "localhost"
+    TCP_PORT = 9009
+    conn = None
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect((TCP_IP,TCP_PORT))
+    s.send(b"Logging in")
+
+    while True:
+        received = s.recv(1024).decode("UTF-8")
+        if received is not None:
+            data = {'number':received}
+            producer.send('twitterEng', value=data)
+            sleep(1)
+    '''
+    '''for e in range(1000):
         try:
             # Produce line (without newline)
+
+
             data = {'number': 'bolsonaro oioi'}
             print("sendinggg")
             producer.send('twitterEng', value=data)
             sleep(2)
         except BufferError:
             sys.stderr.write('%% Local producer queue is full (%d messages awaiting delivery): try again\n')
-
+    '''
         # Serve delivery callback queue.
         # NOTE: Since produce() is an asynchronous API this poll() call
         #       will most likely not serve the delivery callback for the
