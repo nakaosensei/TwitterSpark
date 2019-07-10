8019968c7348a1b08f33980396587cdc74314b15
nakaosensei <nakaosensei@gmail.com>
2019-07-10 19:53:15 -0300

    two topics

--- spark_publisherEn.py
+++ spark_publisherEn.py
@@ -1,3 +1,25 @@
+#!/usr/bin/env python
+#
+# Copyright 2016 Confluent Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+#
+# Example Kafka Producer.
+# Reads lines from stdin and sends to Kafka.
+#
+
 from confluent_kafka import Producer
 import sys
 import socket
@@ -19,7 +41,6 @@ def preProcessData(data):
     data = data.replace('Ã', 'A')
     data = data.replace('â', 'a')
     data = data.replace('Â', 'A')
-
     data = data.replace('è', 'e')
     data = data.replace('È', 'E')
     data = data.replace('é', 'e')
@@ -28,7 +49,6 @@ def preProcessData(data):
     data = data.replace('Ẽ', 'E')
     data = data.replace('ê', 'e')
     data = data.replace('Ê', 'E')
-
     data = data.replace('ì', 'i')
     data = data.replace('Ì', 'I')
     data = data.replace('í', 'i')
@@ -37,7 +57,6 @@ def preProcessData(data):
     data = data.replace('Ĩ', 'I')
     data = data.replace('î', 'i')
     data = data.replace('Î', 'I')
-
     data = data.replace('ò', 'o')
     data = data.replace('Ò', 'O')
     data = data.replace('ó', 'o')
@@ -46,7 +65,6 @@ def preProcessData(data):
     data = data.replace('Õ', 'O')
     data = data.replace('ô', 'o')
     data = data.replace('Ô', 'O')
-    data = data.replace("…","")
     data = data.replace('ù', 'u')
     data = data.replace('Ù', 'U')
     data = data.replace('ú', 'u')
@@ -56,6 +74,7 @@ def preProcessData(data):
     data = data.replace('û', 'u')
     data = data.replace('Û', 'U')
     data = data.replace("...","")
+    data = data.replace("…", "")
     data = data.replace(".", "")
     data = data.replace("?", "")
     data = data.replace("!", "")
@@ -85,6 +104,7 @@ class MyStreamListener(tweepy.StreamListener):
 
     def on_error(self, status_code):
         if status_code == 420:
+            # returning False in on_data disconnects the stream
             return False
 
     def on_status(self, status):
@@ -94,7 +114,7 @@ class MyStreamListener(tweepy.StreamListener):
             f.write(preProcessData(status.text)+"\n")
             f.close()
             data = {'number': preProcessData(status.text)}
-            producer.send('twitterPt', value=data)
+            producer.send('twitterEng', value=data)
             sleep(15)
         except:
             print("error")
@@ -116,11 +136,12 @@ if __name__ == '__main__':
     myStreamListener = MyStreamListener()
     auth = tweepy.OAuthHandler('ZfyYT762f8iptmyOAQiPVpSSG', 'abdgza7zIl2lbRaYdY4L7C5zhGgxftLiACt5WJfrFtGnNIQqIF')
     auth.set_access_token('188507287-370vFKYm0sA6OumKatEWXRZuA4ippzCl7kteJCpi','MIaKNuYorBD5qqsxzzb58gOuvrBuGBToya5nzLEHsJ8kT')
+
     api = tweepy.API(auth)
     myStream = tweepy.Stream(auth=api.auth, listener=myStreamListener)
     while True:
         try:
-            myStream.filter(track=['@jairbolsonaro', 'Jair Bolsonaro', 'jair bolsonaro', 'JAIR BOLSONARO'],languages=['pt'],stall_warnings=True)
+            myStream.filter(track=['@jairbolsonaro', 'Jair Bolsonaro', 'jair bolsonaro', 'JAIR BOLSONARO'],languages=['en'],stall_warnings=True)
         except (ProtocolError, AttributeError):
             continue
 
--- spark_subscriberEn.py
+++ spark_subscriberEn.py
@@ -2,10 +2,15 @@ import os
 import findspark
 findspark.init('../TwitterSpark/lib/spark-2.4.3-bin-hadoop2.7')
 os.environ['PYSPARK_SUBMIT_ARGS'] = '--packages org.apache.spark:spark-streaming-kafka-0-8_2.11:2.4.3 pyspark-shell'
+# initiate Apache Kafka
+# download link: https://www.apache.org/dyn/closer.lua/spark/spark-2.4.3/spark-2.4.3-bin-hadoop2.7.tgz
+# extract downloaded file and put path to it in the following line
 
 from pyspark import SparkContext, SparkConf
 from pyspark.streaming import StreamingContext
 from pyspark.streaming.kafka import KafkaUtils
+
+
 
 
 if __name__ == "__main__":
@@ -13,7 +18,10 @@ if __name__ == "__main__":
     sc.setLogLevel("WARN")
     ssc = StreamingContext(sc, 15)  # 2 second window
 
-    kafkaStream = KafkaUtils.createDirectStream(ssc, ["twitterPt"], {
+    # twitter_eng_stream = KafkaUtils.createStream(ssc, 'localhost:2181', 'spark-streaming', {'twitterEng': 1})
+    # twitter_pt_stream = KafkaUtils.createStream(ssc, 'localhost:2181', 'spark-streaming', {'twitterPt': 1})
+
+    kafkaStream = KafkaUtils.createDirectStream(ssc, ["twitterEng"], {
         'bootstrap.servers': 'localhost:9092',
         'group.id': 'data-mining-group',
         'fetch.message.max.bytes': '15728640',
@@ -22,9 +30,8 @@ if __name__ == "__main__":
 
     lines = kafkaStream.map(lambda x: x[1])
     counts = lines.flatMap(lambda line: line.split(" ")).map(lambda word: (word, 1)).reduceByKey(lambda a, b: a+b)
-    #print(type(counts))
-    counts.pprint()
 
+    counts.pprint()
 
     ssc.start()
     ssc.awaitTermination()
